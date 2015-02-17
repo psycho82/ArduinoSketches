e4a09c0c69395fb29211b8836627f5984dbb79cb
psycho82 <raffaele.maietta@gmail.com>
2015-02-17 01:01:06 +0100

    Echo su seriale quasi finito

--- SERIAL_ECHO/SERIAL_ECHO.ino
+++ SERIAL_ECHO/SERIAL_ECHO.ino
@@ -1,20 +1,32 @@
-String content = "";
-char character;
+String readData = ""; //string contenente i dati letti dalla seriale
 char endChar = ';'; //carattere di fine stringa
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Boot...");
+  Serial.println("Booting...");
 }
 
 void loop() {
-  while(Serial.available()) {
-      character = Serial.read();
-      if (character == endChar) {
-        Serial.println(content);
-        content = "";
-      }else{
-        content.concat(character);
+  readFromSerial(readData,endChar);
+  writeToSerial(readData);
+}
+
+//resta in ascolto sulla seriale, legge dalla seriale e termina la lettura appena riceve il carattere di fine stringa (endChar). Svuota la stringa str passata per riferimento,
+void readFromSerial(String &str, char &endC){
+  char lastChar = '\0';
+  str = "";
+  while(lastChar!=endC) {
+    if(Serial.available()){
+      lastChar = Serial.read();//legge e svuota il buffer di lettura (1byte alla volta)
+      if(lastChar != endC){
+        str.concat(lastChar);
       }
+    }
   }
+}
+
+//Scrive sulla seriale
+void writeToSerial(String &str){
+  Serial.println(str);
+  Serial.flush();//Attende che dall'altra parte vengano letti tutti i dati presenti in uscita.
 }
--- SPI_EEPROM/SPI_EEPROM.ino
+++ SPI_EEPROM/SPI_EEPROM.ino
@@ -1,11 +1,11 @@
 #include <SPI.h>
-//opcodes 2
+
 #define DATAOUT 11//MOSI
 #define DATAIN  12//MISO 
 #define SPICLOCK  13//sck
 #define SLAVESELECT 10//ss
 
-
+//opcodes
 #define WREN  6
 #define WRDI  4
 #define RDSR  5
@@ -21,7 +21,7 @@ char buffer [128]; //buffer dei dati
 // SPCR = 01010000
 //interrupt disabled,spi enabled,msb 1st,master,clk low when idle,
 //sample on leading edge of clk,system clock/4 rate (fastest)
-SPCR = (1<<SPE)|(1<<MSTR);
+//SPCR = (1<<SPE)|(1<<MSTR);
 
 
 
@@ -32,14 +32,17 @@ char endChar = ';'; //carattere di fine stringa
 void setup() {
   Serial.begin(9600);
   Serial.println("Boot...");
+  /*
   pinMode(DATAOUT, OUTPUT);
   pinMode(DATAIN, INPUT);
   pinMode(SPICLOCK,OUTPUT);
   pinMode(SLAVESELECT,OUTPUT);
-  digitalWrite(SLAVESELECT,HIGH); //disable device 
+  digitalWrite(SLAVESELECT,HIGH); //disable device
+  */
 }
 
 void loop() {
+  /*
   while(Serial.available()) {
       character = Serial.read();
       if (character == endChar) {
@@ -49,4 +52,24 @@ void loop() {
         content.concat(character);
       }
   }
+  */
+  content = readFromSerial();
+  writeToSerial(content);
+}
+
+String readFromSerial(){
+  String readData = "";
+  char readChar;
+  while(Serial.available()) {
+      readChar = Serial.read();
+      if (readChar != endChar) {
+        readData.concat(readChar);
+      }
+  }
+  return readData;
+}
+
+void writeToSerial(String str){
+  Serial.println(str);
+  Serial.flush();
 }
